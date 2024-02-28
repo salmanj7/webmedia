fd28335c373589948b66abdaa79fb796c51e2d05
Salman Jamal <salman.jamal@codinghands.in>
2024-02-28 11:34:58 +0530

    admin seeder

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -14,9 +14,12 @@ class DatabaseSeeder extends Seeder
     {
         // \App\Models\User::factory(10)->create();
 
-        // \App\Models\User::factory()->create([
-        //     'name' => 'Test User',
-        //     'email' => 'test@example.com',
-        // ]);
+        \App\Models\User::factory()->create([
+            'name' => 'Super Admin',
+            'email' => 'test@example.com',
+            'password' => bcrypt('12345678'),
+            'is_admin' => true
+
+        ]);
     }
 }
